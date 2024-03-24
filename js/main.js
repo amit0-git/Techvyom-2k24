27dad209ab875051f2cee3c4846bf0817e5f081f
Amit Verma <root.avanti@gmail.com>
2024-03-24 20:26:40 +0530

    24-3-24

---
+++ js/main.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', () => {
+    const earth = document.querySelector('#earth');
+    // Set the final position of the Earth
+    earth.style.width="1500px"
+    earth.style.animationDuration="70s"
+    earth.style.bottom = '-1000px'; // Adjust according to the size of the image
+  });
