e33a0c68e92a186a16f012c8ffea16c48f43f0b3
Amit Verma <root.avanti@gmail.com>
2024-03-25 14:14:49 +0530

    Events Desc

---
+++ js/matrix.js
@@ -0,0 +1,60 @@
+
+function generateRandomChar() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+';
+    return characters.charAt(Math.floor(Math.random() * characters.length));
+}
+
+// Function to update the loading animation
+function updateLoadingAnimation(textDiv, length1) {
+
+    textDiv.innerHTML = '';
+
+    // Generate random characters and append them to the loading animation
+
+    for (let i = 0; i < length1; i++) {
+        textDiv.innerHTML += generateRandomChar();
+    }
+}
+
+
+
+
+function randomTextAnimation(text, textDiv) {
+
+    const interval = setInterval(() => {
+        updateLoadingAnimation(textDiv, text.length);
+    }, 60);
+
+    // Clear the interval after 3 seconds
+    setTimeout(() => {
+        clearInterval(interval);
+        //update the div
+
+        textDiv.innerHTML = text;
+        console.log("Animation stopped")
+    }, 700);
+
+
+
+
+}
+
+
+
+
+
+
+
+const eventh1 = document.getElementById("eventh1");
+
+
+
+eventh1.addEventListener("mouseenter", () => {
+    randomTextAnimation("Events", eventh1)
+});
+
+
+
+
+
+
